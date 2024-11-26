c13f02608abe809a4ee32561572b1bc2b919e58b
Amitai Saiger <asaiger@binghamton.edu>
2024-11-26 16:12:18 -0500

    made obstacle class into a sprite

--- template_final_project-master/src/obstacle.py
+++ template_final_project-master/src/obstacle.py
@@ -1,6 +1,8 @@
-class Obstacle:
+import pygame
+class Obstacle(pygame.sprite.Sprite):
     
     def __init__(self,x, y, img_file):
+        super().__init__()
         """
         Initializes the Obstacle object
         args:
@@ -8,23 +10,18 @@ class Obstacle:
         - y : int - starting y coordinate
         - img_file : str - path to img file
         """
-        self.x = x
-        self.y = y
-        self.img = img_file
+        self.image = pygame.image.load(img_file)
+        self.rect= self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         self.xvel = 0
         self.yvel = 0
-    def move():
+    def move(self):
         '''
         makes the obstacle move toward the runner
         args: none
         return: none
-        NOT YET IMPLENTED
         '''
-    def is_touching():
-        '''
-        checks if the obstacle has hit the runner
-        args: none
-        return: none
-        NOT YET IMPLENTED
-        '''
+        self.rect.x -= self.xvel
+
 
